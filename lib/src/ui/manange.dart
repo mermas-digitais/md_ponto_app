bc1e3f65f21e300c98e26c3d1568ae8a15f38ec9
Vinícius Schneider <mrschneider.v@gmail.com>
2023-08-17 15:58:03 -0300

    Create manangePage

---
+++ lib/src/ui/manange.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ManangePage extends StatefulWidget {
+  const ManangePage({super.key});
+
+  @override
+  State<ManangePage> createState() => _ManangePageState();
+}
+
+class _ManangePageState extends State<ManangePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: const SizedBox(
+          height: 200,
+          width: 200,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.abc),
+      ),
+    );
+  }
+}
