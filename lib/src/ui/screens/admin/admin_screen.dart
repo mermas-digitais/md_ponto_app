9be109e692f209539b5cfdfb775aa1ee75c6e9e3
Vinícius Schneider <mrschneider.v@gmail.com>
2023-09-12 11:48:25 -0300

    feat: create admin_screen

--- lib/src/ui/screens/admin/admin_screen.dart
+++ lib/src/ui/screens/admin/admin_screen.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:md_ponto_app/src/ui/components/molecules/app_bar/app_bar.dart';
+import 'package:md_ponto_app/src/ui/components/organisms/scaffold/app_scaffold.dart';
 
-class ManangePage extends StatefulWidget {
-  const ManangePage({super.key});
+import '../../old_components/user_registration_form.dart';
+
+class AdminPage extends StatefulWidget {
+  const AdminPage({super.key});
 
   @override
-  State<ManangePage> createState() => _ManangePageState();
+  State<AdminPage> createState() => _AdminPageState();
 }
 
-class _ManangePageState extends State<ManangePage> {
+class _AdminPageState extends State<AdminPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
+    return CustomScaffold.withAppBarAndFloatingActionButton(
+        moveUpWhenKeyboardIsOpen: false,
+        appBar: CustomAppBar.withTitleAndBackButton(
+          context: context,
+          title: 'Admin Page',
         ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.background,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.background,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(40),
+              topRight: Radius.circular(40),
+            ),
           ),
-        ),
 
-        //child is a listview with users informations
-        child: Container(
-          color: Theme.of(context).colorScheme.primary,
-          height: 200,
-          width: 200,
+          //child is a listview with users informations
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.abc),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          onPressed: () {
+            //show dialog user_registration_form
+            showDialog(
+                context: context,
+                builder: (context) => const UserRegistrationForm());
+          },
+          child: Icon(
+            Icons.add,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ));
   }
 }
